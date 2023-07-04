328e29a67ca056acb2eaf86d46b943c2a2d062cf
firerum <ademuyiwaadewuyi@gmail.com>
2023-07-04 18:51:01 +0100

    navigation WIP

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -31,7 +31,7 @@ const Nav = () => {
     }, []);
 
     return (
-        <header className="px-6 py-3 md:px-16 bg-pry-purple shadow-sm fixed left-0 top-0 right-0 flex justify-between items-center">
+        <header className="px-6 py-3 md:px-10 bg-pry-purple shadow-sm fixed left-0 top-0 right-0 flex justify-between items-center">
             <h1 className="flex justify-between mb-0 text-lg">
                 <Link href="/">
                     {/* <Image
