953500ae429f9b00c96aa7f95afaccb55d590084
firerum <ademuyiwaadewuyi@gmail.com>
2023-07-09 16:56:05 +0100

    hover effect on creat account

--- src/sections/landing-page/CreateAccountCard.jsx
+++ src/sections/landing-page/CreateAccountCard.jsx
@@ -1,12 +1,40 @@
+import { styled } from 'styled-components';
+
+const _Button = styled.button`
+    span:last-child {
+        color: #807c89;
+    }
+
+    &:hover {
+        background-image: linear-gradient(
+            180deg,
+            #f9e8be 0%,
+            #8a7980 0.59%,
+            #29194a 90%
+        );
+
+        span {
+            color: #fefbf4;
+        }
+
+        span:nth-child(2) {
+            color: #f8e4b2;
+        }
+    }
+`;
+
 export const CreateAccountCard = ({ title, step }) => {
     return (
-        <div className="bg-pry-purple text-left cursor-pointer py-8 px-4 rounded-xl max-w-md">
-            <header className="font-general mb-4">{step}</header>
-            <h3 className="font-semibold">{title}</h3>
-            <p className="hidden md:block text-sm">
+        <_Button
+            className="bg-pry-purple text-left cursor-pointer py-8 px-6 rounded-xl max-w-md"
+            onClick={() => console.log('clicked')}
+        >
+            <span className="block font-general mb-4">{step}</span>
+            <span className="block font-semibold mb-6">{title}</span>
+            <span className="hidden md:block text-sm">
                 You’ll be asked to submit your name, email address and other
                 relevant information
-            </p>
-        </div>
+            </span>
+        </_Button>
     );
 };
--- src/sections/landing-page/Home.jsx
+++ src/sections/landing-page/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
             <section className="banner bg-pry-purple text-center xl:h-screen pt-24 px-6 md:px-16 -mx-6 md:-mx-16 ">
                 <div>
                     <h1>
-                        <span className="text-xs font-semibold md:text-base rounded-3xl py-2 px-6 mb-4 inline-block award-wining text-secondary-gold">
+                        <span className="text-xs font-semibold md:text-base rounded-3xl py-2 px-6 mb-4 inline-block bg-[#fefbf4] text-secondary-gold">
                             #1 Award-winning memory curation platform
                         </span>
                         <br />
