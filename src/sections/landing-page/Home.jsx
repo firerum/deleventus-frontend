b654668db9cec79e0b70dd18825f892ea2b97e86
firerum <ademuyiwaadewuyi@gmail.com>
2023-07-18 03:43:01 +0100

    wip

--- src/sections/landing-page/Home.jsx
+++ src/sections/landing-page/Home.jsx
@@ -13,7 +13,7 @@ import { FirstProcess } from './getting-started/FirstProcess';
 import { SecondProcess } from './getting-started/SecondProcess';
 import { ThirdProcess } from './getting-started/ThirdProcess';
 import { FourthProcess } from './getting-started/FourthProcess';
-import { Banner, HomeAnimation } from './Animation/HomeAnimation';
+import { HomeAnimation } from './Animation/HomeAnimation';
 import { motion } from 'framer-motion';
 
 const processCard = [
@@ -52,9 +52,9 @@ export default function Home() {
     const [count, setCount] = useState(0);
 
     return (
-        <HomeAnimation>
-            <div className="px-6 md:text-center md:px-16">
-                <section className="banner bg-pry-purple text-center xl:h-screen pt-24 px-6 md:px-16 -mx-6 md:-mx-16 ">
+        <div className="px-6 md:text-center md:px-16">
+            <section className="banner bg-pry-purple text-center xl:h-screen pt-24 px-6 md:px-16 -mx-6 md:-mx-16 ">
+                <HomeAnimation>
                     <div>
                         <motion.h1>
                             <span className="text-xs font-semibold md:text-base rounded-3xl py-2 px-6 mb-4 inline-block bg-[#fefbf4] text-secondary-gold">
@@ -75,149 +75,148 @@ export default function Home() {
                             bringing your events to life.
                         </p>
                     </div>
-                    <div className="flex flex-col justify-center md:flex-row gap-4 mt-8">
+                </HomeAnimation>
+                <div className="flex flex-col justify-center md:flex-row gap-4 mt-8">
+                    <Link
+                        href="/signup"
+                        className="py-4 px-8 font-medium rounded-default text-pry-purple bg-btn-color"
+                    >
+                        Get Started
+                    </Link>
+                    <Button outlined={'true'} title="Watch Demo" />
+                </div>
+            </section>
+            <section className="">
+                <div className="mb-20 xxl:grid grid-cols-2 justify-between xxl:text-left">
+                    <h2 className="xxl:w-2/3">
+                        Getting started with us is easy and free
+                    </h2>
+                    <p>
+                        Getting started is a breeze - sign up for free and
+                        embark on your event planning venture. Start your event
+                        planning journey with ease and unlock the potential of
+                        our platform.
+                    </p>
+                </div>
+                <div className="pointer-events-none">
+                    <CreateAccountCarousel count={count}>
+                        <FirstProcess />
+                        {/* slide 2 */}
+                        <SecondProcess />
+                        {/* slide 3 */}
+                        <ThirdProcess />
+                        {/* slide 4 */}
+                        <FourthProcess />
+                    </CreateAccountCarousel>
+                </div>
+                <div className="grid grid-cols-2 gap-4 py-8 xl:grid-cols-3 xxl:grid-cols-4">
+                    {processCard.map((pc, index) => (
+                        <CreateAccountCard
+                            key={pc.id}
+                            title={pc.title}
+                            step={pc.step}
+                            content={pc.content}
+                            setCount={setCount}
+                            index={index}
+                            count={count}
+                        />
+                    ))}
+                </div>
+            </section>
+            <section className="bg-pry-purple px-6 md:px-16 -mx-6 md:-mx-16 ">
+                <div className="xxl:grid grid-cols-2 xxl:text-left mb-16">
+                    <h2>Featured Events</h2>
+                    <p>
+                        Explore a curated collection of remarkable gatherings,
+                        conferences, parties, fundraisers, and more. Whether
+                        you're looking for networking opportunities,
+                        entertainment, or a chance to support a worthy cause,
+                        our featured events offer something for everyone.
+                    </p>
+                </div>
+                <div className="">
+                    <Carousel>
+                        {eventData.map((data, index) => (
+                            <EventCard
+                                key={index}
+                                name={data.name}
+                                desc={data.desc}
+                                date={data.date_of_event}
+                                avatar={data.avatar}
+                            />
+                        ))}
+                    </Carousel>
+                </div>
+                <div className="swiper-custom-pagination"></div>
+            </section>
+            <section className="">
+                <div className="xxl:grid grid-cols-2 xxl:text-left mb-12">
+                    <h2>What Our Clients Say</h2>
+                    <p>
+                        We take pride in providing a seamless event planning
+                        experience for our clients. Don't just take our word for
+                        it—see what our clients have to say about their
+                        experiences with Deleventus.
+                    </p>
+                </div>
+                <div className="">
+                    <Carousel>
+                        {testimonialData.map((data, index) => (
+                            <Testimonial
+                                key={index}
+                                name={data.name}
+                                title={data.title}
+                                content={data.content}
+                                avatar={data.image}
+                            />
+                        ))}
+                    </Carousel>
+                </div>
+            </section>
+            <section className="max-w-3xl mx-auto faq">
+                <div className="mb-16">
+                    <h2>Frequently Asked Questions</h2>
+                    <p>
+                        Welcome to our FAQ section where we address common
+                        queries about Deleventus. Find answers to your questions
+                        below, and if you don't see what you're looking for,
+                        feel free to reach out to our support team for further
+                        assistance.
+                    </p>
+                </div>
+                <div>
+                    {faqs.map((faq, index) => (
+                        <Faq
+                            key={index}
+                            title={faq.question}
+                            content={faq.answer}
+                            index={index}
+                        />
+                    ))}
+                </div>
+            </section>
+            <section className="text-center bg-pry-purple py-16 px-6 md:px-16 -mx-6 md:-mx-16">
+                <div className="font-medium py-12 px-6 max-w-3xl mx-auto rounded-lg contact">
+                    <h2 className="text-pry-purple">
+                        Get started with Deleventus today
+                    </h2>
+                    <p className="text-sm text-contact-text-color font-light md:text-base md:w-3/4 md:mx-auto">
+                        Join Deleventus today to curate unforgettable event
+                        memories. Capture and share photos/videos, manage ticket
+                        sales, engage with attendees, and preserve valuable
+                        remarks. Say goodbye to scattered albums and fragmented
+                        documentation.
+                    </p>
+                    <div className="mt-8">
                         <Link
                             href="/signup"
                             className="py-4 px-8 font-medium rounded-default text-pry-purple bg-btn-color"
                         >
-                            Get Started
+                            Get Started Free
                         </Link>
-                        <Button outlined={'true'} title="Watch Demo" />
                     </div>
-                </section>
-                <section className="">
-                    <div className="mb-20 xxl:grid grid-cols-2 justify-between xxl:text-left">
-                        <h2 className="xxl:w-2/3">
-                            Getting started with us is easy and free
-                        </h2>
-                        <p>
-                            Getting started is a breeze - sign up for free and
-                            embark on your event planning venture. Start your
-                            event planning journey with ease and unlock the
-                            potential of our platform.
-                        </p>
-                    </div>
-                    <div className="pointer-events-none">
-                        <CreateAccountCarousel count={count}>
-                            <FirstProcess />
-                            {/* slide 2 */}
-                            <SecondProcess />
-                            {/* slide 3 */}
-                            <ThirdProcess />
-                            {/* slide 4 */}
-                            <FourthProcess />
-                        </CreateAccountCarousel>
-                    </div>
-                    <div className="grid grid-cols-2 gap-4 py-8 xl:grid-cols-3 xxl:grid-cols-4">
-                        {processCard.map((pc, index) => (
-                            <CreateAccountCard
-                                key={pc.id}
-                                title={pc.title}
-                                step={pc.step}
-                                content={pc.content}
-                                setCount={setCount}
-                                index={index}
-                                count={count}
-                            />
-                        ))}
-                    </div>
-                </section>
-                <section className="bg-pry-purple px-6 md:px-16 -mx-6 md:-mx-16 ">
-                    <div className="xxl:grid grid-cols-2 xxl:text-left mb-16">
-                        <h2>Featured Events</h2>
-                        <p>
-                            Explore a curated collection of remarkable
-                            gatherings, conferences, parties, fundraisers, and
-                            more. Whether you're looking for networking
-                            opportunities, entertainment, or a chance to support
-                            a worthy cause, our featured events offer something
-                            for everyone.
-                        </p>
-                    </div>
-                    <div className="">
-                        <Carousel>
-                            {eventData.map((data, index) => (
-                                <EventCard
-                                    key={index}
-                                    name={data.name}
-                                    desc={data.desc}
-                                    date={data.date_of_event}
-                                    avatar={data.avatar}
-                                />
-                            ))}
-                        </Carousel>
-                    </div>
-                    <div className="swiper-custom-pagination"></div>
-                </section>
-                <section className="">
-                    <div className="xxl:grid grid-cols-2 xxl:text-left mb-12">
-                        <h2>What Our Clients Say</h2>
-                        <p>
-                            We take pride in providing a seamless event planning
-                            experience for our clients. Don't just take our word
-                            for it—see what our clients have to say about their
-                            experiences with Deleventus.
-                        </p>
-                    </div>
-                    <div className="">
-                        <Carousel>
-                            {testimonialData.map((data, index) => (
-                                <Testimonial
-                                    key={index}
-                                    name={data.name}
-                                    title={data.title}
-                                    content={data.content}
-                                    avatar={data.image}
-                                />
-                            ))}
-                        </Carousel>
-                    </div>
-                </section>
-                <section className="max-w-3xl mx-auto faq">
-                    <div className="mb-16">
-                        <h2>Frequently Asked Questions</h2>
-                        <p>
-                            Welcome to our FAQ section where we address common
-                            queries about Deleventus. Find answers to your
-                            questions below, and if you don't see what you're
-                            looking for, feel free to reach out to our support
-                            team for further assistance.
-                        </p>
-                    </div>
-                    <div>
-                        {faqs.map((faq, index) => (
-                            <Faq
-                                key={index}
-                                title={faq.question}
-                                content={faq.answer}
-                                index={index}
-                            />
-                        ))}
-                    </div>
-                </section>
-                <section className="text-center bg-pry-purple py-16 px-6 md:px-16 -mx-6 md:-mx-16">
-                    <div className="font-medium py-12 px-6 max-w-3xl mx-auto rounded-lg contact">
-                        <h2 className="text-pry-purple">
-                            Get started with Deleventus today
-                        </h2>
-                        <p className="text-sm text-contact-text-color font-light md:text-base md:w-3/4 md:mx-auto">
-                            Join Deleventus today to curate unforgettable event
-                            memories. Capture and share photos/videos, manage
-                            ticket sales, engage with attendees, and preserve
-                            valuable remarks. Say goodbye to scattered albums
-                            and fragmented documentation.
-                        </p>
-                        <div className="mt-8">
-                            <Link
-                                href="/signup"
-                                className="py-4 px-8 font-medium rounded-default text-pry-purple bg-btn-color"
-                            >
-                                Get Started Free
-                            </Link>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </HomeAnimation>
+                </div>
+            </section>
+        </div>
     );
 }
