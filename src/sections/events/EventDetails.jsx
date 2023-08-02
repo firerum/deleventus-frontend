f4b3bdcdce3bb37f1de7d34eaa07a227898270b7
firerum <ademuyiwaadewuyi@gmail.com>
2023-08-03 00:16:30 +0100

    event details guest/event-owner view wip

--- src/sections/events/EventDetails.jsx
+++ src/sections/events/EventDetails.jsx
@@ -6,6 +6,8 @@ import { useParams } from 'next/navigation';
 import { EventGuests } from './EventGuests';
 import { EventMedia } from './EventMedia';
 import { EventOverview } from './EventOverview';
+import { Button } from '@components/Button';
+import { FaPencilAlt, FaShareAlt } from 'react-icons/fa';
 
 export default function EventDetails() {
     const [tab, setTab] = useState('Overview');
@@ -13,9 +15,21 @@ export default function EventDetails() {
 
     return (
         <div className="p-6 lg:p-0 lg:pr-6">
-            <div className="py-4">
-                <span className="text-[#726D7C]">My Events </span>&gt;{' '}
-                <span className="font-bold">Event Name</span>
+            <div className="flex justify-between items-center py-4">
+                <div>
+                    <span className="text-[#726D7C]">My Events </span>&gt;{' '}
+                    <span className="font-bold">Event Name</span>
+                </div>
+                <div className="flex flex-col md:flex-row gap-2 text-xs md:text-sm">
+                    <Button className="border-1 px-4 py-2 flex items-center gap-2 rounded-default">
+                        <span className="order-2">Share Event</span>
+                        <FaShareAlt />
+                    </Button>
+                    <Button className="bg-btn-color text-white px-4 py-2 flex items-center gap-2 rounded-default">
+                        <span className="order-2"> Edit Event</span>
+                        <FaPencilAlt />
+                    </Button>
+                </div>
             </div>
             <section className="hidden lg:block">
                 <h2 className="text-xl pb-2">Events Summary</h2>
