1a32d59afeed50f953f4d964ea08a2b4303aa488
firerum <ademuyiwaadewuyi@gmail.com>
2023-08-14 20:31:28 +0100

    remove tab swipe

--- src/sections/landing-page/Explore.jsx
+++ src/sections/landing-page/Explore.jsx
@@ -4,7 +4,6 @@ import { WebAppSubnav } from '@layouts/WebAppSubnav';
 import { EventCard, eventData } from '@sections/events/EventCard';
 import Link from 'next/link';
 import { AnimateContent } from '@utils/framer-motion/AnimateContent';
-import { TabSwipe } from '@utils/tab-swipe';
 
 export default function Explore() {
     const tabs = [
@@ -17,7 +16,6 @@ export default function Explore() {
         'Other',
     ];
     const [tab, setTab] = useState(tabs[0]);
-    const [swipedTabIndex, setSwipedTabIndex] = useState(0);
 
     return (
         <main className="bg-pry-purple min-h-screen text-center pt-20 pb-12 px-6 md:px-16 overflow-clip">
@@ -26,42 +24,35 @@ export default function Explore() {
                     <WebAppSubnav tabs={tabs} setTab={setTab} tab={tab} />
                 </div>
                 <div className="mt-6">
-                    <TabSwipe
-                        tabs={tabs}
-                        setTab={setTab}
-                        swipedTabIndex={swipedTabIndex}
-                        setSwipedTabIndex={setSwipedTabIndex}
-                    >
-                        {tab === 'All' && (
-                            <AnimateContent>
-                                <div className="flex flex-wrap justify-center gap-4 lg:grid grid-cols-2 xl:grid-cols-4">
-                                    {eventData.map((eve, index) => (
-                                        <Link
-                                            key={index}
-                                            href={`/explore/${eve.name}`}
-                                            className="hover:no-underline"
-                                        >
-                                            <EventCard
-                                                name={eve.name}
-                                                desc={eve.desc}
-                                                date={eve.date_of_event}
-                                                avatar={eve.avatar}
-                                            />
-                                        </Link>
-                                    ))}
-                                </div>
-                            </AnimateContent>
-                        )}
-                        {tab === 'Wedding' && <div>All Public Weddings</div>}
-                        {tab === 'Birthday' && <div>All Public Birthdays</div>}
-                        {tab === 'Anniversary' && (
-                            <div>All Public Anniversaries</div>
-                        )}
-                        {tab === 'Convocation' && (
-                            <div>All Public Convocations</div>
-                        )}
-                        {tab === 'Other' && <div>All Other Events</div>}
-                    </TabSwipe>
+                    {tab === 'All' && (
+                        <AnimateContent>
+                            <div className="flex flex-wrap justify-center gap-4 lg:grid grid-cols-2 xl:grid-cols-4">
+                                {eventData.map((eve, index) => (
+                                    <Link
+                                        key={index}
+                                        href={`/explore/${eve.name}`}
+                                        className="hover:no-underline"
+                                    >
+                                        <EventCard
+                                            name={eve.name}
+                                            desc={eve.desc}
+                                            date={eve.date_of_event}
+                                            avatar={eve.avatar}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
+                        </AnimateContent>
+                    )}
+                    {tab === 'Wedding' && <div>All Public Weddings</div>}
+                    {tab === 'Birthday' && <div>All Public Birthdays</div>}
+                    {tab === 'Anniversary' && (
+                        <div>All Public Anniversaries</div>
+                    )}
+                    {tab === 'Convocation' && (
+                        <div>All Public Convocations</div>
+                    )}
+                    {tab === 'Other' && <div>All Other Events</div>}
                 </div>
             </section>
         </main>
--- src/sections/users/Timeline.jsx
+++ src/sections/users/Timeline.jsx
@@ -8,7 +8,6 @@ import { CreateEvent } from '@sections/events/CreateEvent';
 import { MdCreate } from 'react-icons/md';
 import Link from 'next/link';
 import { AnimateContent } from '@utils/framer-motion/AnimateContent';
-import { TabSwipe } from '@utils/tab-swipe';
 
 export default function Timeline() {
     const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +20,6 @@ export default function Timeline() {
         'Other',
     ];
     const [tab, setTab] = useState(tabs[0]);
-    const [swipedTabIndex, setSwipedTabIndex] = useState(0);
 
     return (
         <main className="p-6 lg:pl-0 lg:pr-6">
@@ -30,51 +28,40 @@ export default function Timeline() {
                     <WebAppSubnav tabs={tabs} setTab={setTab} tab={tab} />
                 </div>
                 <div className="mt-6">
-                    <TabSwipe
-                        tabs={tabs}
-                        setTab={setTab}
-                        swipedTabIndex={swipedTabIndex}
-                        setSwipedTabIndex={setSwipedTabIndex}
-                    >
-                        {tab === 'All' && (
-                            <AnimateContent>
-                                <div className="flex flex-wrap justify-center gap-4 lg:grid grid-cols-2 xl:grid-cols-4">
-                                    {eventData.map((eve, index) => (
-                                        <Link
-                                            href={`events/${eve.name}`}
-                                            key={index}
-                                        >
-                                            <EventCard
-                                                name={eve.name}
-                                                desc={eve.desc}
-                                                date={eve.date_of_event}
-                                                avatar={eve.avatar}
-                                            />
-                                        </Link>
-                                    ))}
-                                </div>
-                            </AnimateContent>
-                        )}
-                        {tab === 'Wedding' && (
-                            <AnimateContent>Public Weddings</AnimateContent>
-                        )}
-                        {tab === 'Birthday' && (
-                            <AnimateContent>
-                                All Public Birthdays
-                            </AnimateContent>
-                        )}
-                        {tab === 'Anniversary' && (
-                            <AnimateContent>
-                                Public Anniversaries
-                            </AnimateContent>
-                        )}
-                        {tab === 'Convocation' && (
-                            <AnimateContent>Public Convocations</AnimateContent>
-                        )}
-                        {tab === 'Other' && (
-                            <AnimateContent>All Other Events</AnimateContent>
-                        )}
-                    </TabSwipe>
+                    {tab === 'All' && (
+                        <AnimateContent>
+                            <div className="flex flex-wrap justify-center gap-4 lg:grid grid-cols-2 xl:grid-cols-4">
+                                {eventData.map((eve, index) => (
+                                    <Link
+                                        href={`events/${eve.name}`}
+                                        key={index}
+                                    >
+                                        <EventCard
+                                            name={eve.name}
+                                            desc={eve.desc}
+                                            date={eve.date_of_event}
+                                            avatar={eve.avatar}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
+                        </AnimateContent>
+                    )}
+                    {tab === 'Wedding' && (
+                        <AnimateContent>Public Weddings</AnimateContent>
+                    )}
+                    {tab === 'Birthday' && (
+                        <AnimateContent>All Public Birthdays</AnimateContent>
+                    )}
+                    {tab === 'Anniversary' && (
+                        <AnimateContent>Public Anniversaries</AnimateContent>
+                    )}
+                    {tab === 'Convocation' && (
+                        <AnimateContent>Public Convocations</AnimateContent>
+                    )}
+                    {tab === 'Other' && (
+                        <AnimateContent>All Other Events</AnimateContent>
+                    )}
                 </div>
                 <Modal isOpen={isOpen} handleClose={() => setIsOpen(false)}>
                     <CreateEvent />
