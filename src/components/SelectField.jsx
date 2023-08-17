c58d637a8255e4e8e3bf594cc30602ef2fbec4fa
firerum <ademuyiwaadewuyi@gmail.com>
2023-08-17 10:53:28 +0100

    select accessibility working well. I want to add arrow down/ arrow up high

--- src/components/SelectField.jsx
+++ src/components/SelectField.jsx
@@ -7,25 +7,9 @@ const DropDownContainer = styled.div`
     width: 100%;
     position: relative;
     border: 1px solid #e7ddfb;
-    padding-block: 0.57rem;
-    padding-inline: 1rem;
     border-radius: 3px;
     margin-bottom: 1rem;
     background-color: #fff;
-
-    &:focus-within {
-        outline: 2px solid #786995;
-    }
-`;
-
-const DropDownHeader = styled.button`
-    background: inherit;
-    text-transform: uppercase;
-    display: block;
-
-    &:focus {
-        outline: 2px solid #786995;
-    }
 `;
 
 const DropDownList = styled.ul`
@@ -59,8 +43,8 @@ const ListItem = styled.li`
 export const SelectField = ({ children, header }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
+    const [focusedOptionIndex, setFocusedOptionIndex] = useState(-1);
     const ref = useRef();
-    const optionItem = useRef();
     const listBox = useRef();
 
     // close select when clicked anywhere outside of it
@@ -77,61 +61,75 @@ export const SelectField = ({ children, header }) => {
     };
 
     // Handle key presses on individual options
-    const handleOptionKeyDown = (event, value) => {
+    const handleOptionKeyDown = (event, index, value) => {
         if (event.key === 'Enter' || event.key === ' ') {
             setSelectedOption(value); // Selects the option on Enter or Space key press
             setIsOpen(false); // Closes the dropdown after selection
+        } else if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            const nextIndex = (index + 1) % children.length;
+            setFocusedOptionIndex(nextIndex);
+        } else if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            const prevIndex = (index - 1 + children.length) % children.length;
+            setFocusedOptionIndex(prevIndex);
         }
     };
 
     return (
         <DropDownContainer ref={ref}>
-            <div
-                className="w-full font-light flex justify-between items-center gap-4"
-                onClick={() => setIsOpen((prev) => !prev)}
+            <button
+                className="w-full py-[0.57rem] px-4 font-light flex justify-between items-center gap-4"
+                tabIndex={0}
+                aria-haspopup="listbox"
+                aria-expanded={isOpen}
+                onClick={(event) => {
+                    event.preventDefault();
+                    setIsOpen((prev) => !prev);
+                }}
+                onKeyDown={(event) => {
+                    if (event.key === 'Enter' || event.key === ' ') {
+                        event.preventDefault();
+                        handleDropdownToggle();
+                    }
+                }}
             >
-                <DropDownHeader
-                    type="button"
-                    aria-haspopup="listbox"
-                    aria-expanded={isOpen}
-                    onClick={() => handleDropdownToggle()}
-                    onKeyDown={(event) => {
-                        if (event.key === 'Enter' || event.key === ' ') {
-                            event.preventDefault();
-                            handleDropdownToggle();
-                        }
-                    }}
-                >
+                <span className="uppercase">
                     {selectedOption ? (
                         <span className="font-normal">{selectedOption}</span>
                     ) : (
                         <span className="title">{header}</span>
                     )}
-                </DropDownHeader>
+                </span>
                 <FaAngleDown
                     className={`transition-all duration-500 linear ${
                         isOpen ? '-rotate-180' : 'rotate-0'
                     }`}
                 />
-            </div>
+            </button>
 
             {isOpen && (
                 <DropDownList
                     role="listbox"
                     ref={listBox}
-                    tabIndex={0}
                     aria-activedescendant={selectedOption}
                 >
                     {children.map((child, index) => (
                         <ListItem
                             key={index}
                             role="option"
-                            ref={optionItem}
+                            tabIndex={0}
                             onClick={onOptionClicked(child)}
                             aria-selected={child === selectedOption}
                             onKeyDown={(event) =>
-                                handleOptionKeyDown(event, child)
+                                handleOptionKeyDown(event, index, child)
                             }
+                            style={{
+                                backgroundColor:
+                                    focusedOptionIndex === index
+                                        ? '#e7ddfb'
+                                        : 'transparent',
+                            }}
                         >
                             {child}
                         </ListItem>
