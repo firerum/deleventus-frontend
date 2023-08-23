1dad3dafcad3d6065594cc4704e2062b226a4829
firerum <ademuyiwaadewuyi@gmail.com>
2023-08-24 00:59:56 +0100

    vertical carousel delay smoothness

--- src/components/Carousels/VerticalCarousel.jsx
+++ src/components/Carousels/VerticalCarousel.jsx
@@ -3,21 +3,20 @@ import { motion } from 'framer-motion';
 export const VerticalCarousel = ({ children, currentIndex }) => {
     return (
         <div className="carousel-container">
-            <motion.div>
-                <motion.div
-                    className="carousel-slide"
-                    key={currentIndex}
-                    custom={currentIndex}
-                    initial={{ y: 80 }}
-                    animate={{ y: 0 }}
-                    transition={{
-                        type: 'spring',
-                        stiffness: 100,
-                        duration: 2,
-                    }}
-                >
-                    {children[currentIndex]}
-                </motion.div>
+            <motion.div
+                className="carousel-slide"
+                key={currentIndex}
+                custom={currentIndex}
+                initial={{ y: 500 }}
+                animate={{ y: 0 }}
+                transition={{
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 24,
+                    duration: 1,
+                }}
+            >
+                {children[currentIndex]}
             </motion.div>
         </div>
     );
