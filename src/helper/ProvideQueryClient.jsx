13f19839ef87f8a6a06d6eb8516d7f580d95ccfd
firerum <ademuyiwaadewuyi@gmail.com>
2023-08-26 22:52:31 +0100

    fucking around

--- src/helper/ProvideQueryClient.jsx
+++ src/helper/ProvideQueryClient.jsx
@@ -1,9 +1,9 @@
 'use client';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
 export const ProvideQueryClient = ({ children }) => {
+    const [queryClient] = useState(() => new QueryClient());
     return (
         <QueryClientProvider client={queryClient}>
             {children}
